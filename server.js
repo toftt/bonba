ddcac93a08b8c36b5f2bdf56e79e31bcb7586e52
Joachim Toft <c.joachim.toft@gmail.com>
2019-02-24 00:31:44 +0100

    last commit but with backend

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-const app = express();
-const port = process.env.PORT || 3001;
 require('dotenv').config();
+const fs = require('fs');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const app = express()
+const server = http.Server(app);
+const io = socketIo(server);
+const port = process.env.PORT || 3001;
 
 app.get('/test', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-const playlists = JSON.parse(fs.readFileSync('./playlists.json'));
-console.log(playlists);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => console.log('user disconnected'));
+    socket.on('nickname', (msg) => console.log(`user sends nickname: ${msg}`));
+    socket.on('request_song', () => {
+      const rand = Math.floor(Math.random() * playlist.length);
+      const song = playlist[rand];
+      console.log(`emitting: ${song.name} by ${song.artists}`);
+      socket.emit('give_song', JSON.stringify(song));
+    });
+});
+
+server.listen(port, () => console.log(`listening on port ${port}`));
+
+const playlist = JSON.parse(fs.readFileSync('./playlists.json')).popList;
